366bd13bfce3dfa647f8761cbe6c5b3fbb8433ef
Yang Liu <foamliu@yeah.net>
2018-06-20 19:19:17 +0800

    update

--- demo.py
+++ demo.py
@@ -1,5 +1,5 @@
-from gaze.nodes.core import EdgeDetection, Sink
 from gaze.nodes import Source
+from gaze.nodes.core import EdgeDetection, Sink
 from gaze.pipes import Pipe
 
 if __name__ == '__main__':
