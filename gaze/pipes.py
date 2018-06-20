40a6983ab99c0e42b2d72d660792cb14d76b0a38
Yang Liu <foamliu@yeah.net>
2018-06-20 21:21:33 +0800

    update

--- gaze/pipes.py
+++ gaze/pipes.py
@@ -4,3 +4,6 @@ from .engine.sequential import Sequential
 class Pipe(Sequential):
     def __init__(self, name=None):
         super(Pipe, self).__init__(name=name)
+
+    def run(self):
+        pass
