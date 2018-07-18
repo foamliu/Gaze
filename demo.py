b7797a02e70871c3a72be35f59829a0f620698b4
Yang Liu <foamliu@yeah.net>
2018-07-18 13:26:09 +0800

    udpate

--- demo.py
+++ demo.py
@@ -1,9 +1,9 @@
-from gaze.nodes import FileSource
+from gaze.nodes import VideoTestSource
 from gaze.nodes.core import EdgeDetection, AutoVideoSink
 from gaze.pipes import Graph
 
 if __name__ == '__main__':
-    x = FileSource('~/code/Gaze/movie.mp4')
+    x = VideoTestSource()
     x = EdgeDetection()(x)
     x = AutoVideoSink()(x)
 
