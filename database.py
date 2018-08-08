4ed49ce05a1dda86b3d57c641f3fc238930f154f
WANG PeiSheng <115681033@qq.com>
2018-08-08 15:41:23 +0800

    add deploy functions
    Use dockerfile to deploy the gaze

--- database.py
+++ database.py
@@ -1,9 +1,10 @@
 from pymongo import MongoClient
-import pprint
+import string,random
 
 client = MongoClient('localhost', 27017)
 db = client.gazedb
 packages = db.packages
+deploys = db.deploys
 
 
 def insert_package(user_name, package_name, description):
@@ -22,3 +23,16 @@ def query_packages():
 def delete_package(package_name):
     packages.remove({"package_name": package_name})
 
+
+def insert_deploy(user_name, package_name):
+    info = {
+        "user_name": user_name,
+        "deploy_name": generate_random_string(8),
+        "package_name": package_name,
+        "status": "creating"
+    }
+    deploys.insert_one(info)
+
+
+def generate_random_string(length):
+    return ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(length))
