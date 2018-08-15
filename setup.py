94ac205ced1a5d40b5807f87668ce30e2e8f14a5
王沛晟 <115681033@qq.com>
2018-08-15 17:26:27 +0800

    change output dir
    change output dir

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="gaze",
-    version="0.0.2",
+    version="0.0.3",
     author="Peisheng Wang",
     author_email="t-peiwa@microsoft.com",
     description="Gaze integrates the computing power of private clouds with data from IoT devices such as cameras,"
