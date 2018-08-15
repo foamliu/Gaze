9236596c802f7cc9855726f6b6fb1bbfa38bf124
Yang Liu <foamliu@yeah.net>
2018-08-15 10:16:20 +0800

    change graph

--- demo.py
+++ demo.py
@@ -1,10 +1,11 @@
-from gaze.nodes import NetworkSource
+from gaze.nodes import NetworkSource, VideoTestSource
 from gaze.nodes.core import EdgeDetection, AutoVideoSink
 from gaze.pipes import Graph
 
 
 if __name__ == '__main__':
-    x = NetworkSource()
+    x = VideoTestSource()
+    x = EdgeDetection()(x)
     x = AutoVideoSink()(x)
 
     graph = Graph(x)
