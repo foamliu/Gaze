7c9c4f7d37ef5f47dc324b5c8e35bc69667d9d57
王沛晟 <115681033@qq.com>
2018-08-15 09:56:14 +0800

    update dockerfile & demo
    update dockerfile & demo

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,8 @@
 FROM base/archlinux
 MAINTAINER Peisheng Wang <115681033@qq.com>
 RUN pacman -Syu --noconfirm
-RUN pacman -S opencv --noconfirm
+RUN pacman -S opencv gtk3 hdf5 python-numpy --noconfirm
 RUN pacman -S gstreamer --noconfirm
-RUN pacman -S python-numpy --noconfirm
 RUN pacman -S python-pip --noconfirm
 RUN pip install gaze
 RUN pacman -S unzip --noconfirm
--- demo.py
+++ demo.py
@@ -1,11 +1,11 @@
-from gaze.nodes import NetworkSource
-from gaze.nodes.core import EdgeDetection, AutoVideoSink
+from gaze.nodes import NetworkSource, VideoTestSource
+from gaze.nodes.core import EdgeDetection, AutoVideoSink, FileSink
 from gaze.pipes import Graph
 
 
 if __name__ == '__main__':
-    x = NetworkSource()
-    x = AutoVideoSink()(x)
+    x = VideoTestSource()
+    x = FileSink()(x)
 
     graph = Graph(x)
     graph.run()
