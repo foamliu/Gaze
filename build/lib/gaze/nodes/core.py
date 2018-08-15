5dd615f87a5177acc60a89011c8bfef6c7369232
王沛晟 <115681033@qq.com>
2018-08-15 13:56:49 +0800

    package gaze-0.0.2
    package gaze-0.0.2

--- build/lib/gaze/nodes/core.py
+++ build/lib/gaze/nodes/core.py
@@ -1,5 +1,6 @@
-from ..engine.base_node import Node
 import cv2 as cv
+
+from ..engine.base_node import Node
 
 
 class EdgeDetection(Node):
@@ -9,7 +10,8 @@ class EdgeDetection(Node):
     def call(self, inputs, **kwargs):
         gray = cv.cvtColor(inputs, cv.COLOR_BGR2GRAY)
         laplacian = cv.Laplacian(gray, cv.CV_8U)
-        return laplacian
+        output = cv.cvtColor(laplacian, cv.COLOR_GRAY2RGB)
+        return output
 
 
 class Sink(Node):
@@ -26,4 +28,15 @@ class AutoVideoSink(Sink):
 
     def call(self, inputs, **kwargs):
         cv.imshow('AutoVideoSink', inputs)
+        return None
+
+
+class FileSink(Sink):
+    def __init__(self, **kwargs):
+        super(FileSink, self).__init__(**kwargs)
+        fourcc = cv.VideoWriter_fourcc(*'MPEG')
+        self.out = cv.VideoWriter(filename='output.avi', fourcc=fourcc, fps=25.0, frameSize=(1280, 720), isColor=True)
+
+    def call(self, inputs, **kwargs):
+        self.out.write(inputs)
         return None
