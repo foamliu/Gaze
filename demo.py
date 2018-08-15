1dced9fdf872738e5c7e9118b47458106b54c1ba
Yang Liu <foamliu@yeah.net>
2018-08-15 10:18:17 +0800

    update filesink

--- demo.py
+++ demo.py
@@ -1,12 +1,12 @@
 from gaze.nodes import NetworkSource, VideoTestSource
-from gaze.nodes.core import EdgeDetection, AutoVideoSink
+from gaze.nodes.core import EdgeDetection, AutoVideoSink, FileSink
 from gaze.pipes import Graph
 
 
 if __name__ == '__main__':
     x = VideoTestSource()
     x = EdgeDetection()(x)
-    x = AutoVideoSink()(x)
+    x = FileSink()(x)
 
     graph = Graph(x)
     graph.run()
