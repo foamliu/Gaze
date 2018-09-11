623c9a93abc397092d3f54c60caf51151092c8e3
Yang Liu <foamliu@yeah.net>
2018-09-11 16:40:16 +0800

    frame rate -> 25.0

--- demo.py
+++ demo.py
@@ -7,7 +7,7 @@ from gaze.pipes import Graph
 if __name__ == '__main__':
     x = NetworkSource()
     x = FaceDetection()(x)
-    x = FileSink()(x)
+    x = AutoVideoSink()(x)
 
     graph = Graph(x)
     graph.run()
