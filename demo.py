925f43e06fd50f1c746b923da7f12e066bc9285a
Yang Liu <foamliu@yeah.net>
2018-09-11 15:58:33 +0800

    change to *'MP4V' codec to be smaller video

--- demo.py
+++ demo.py
@@ -7,7 +7,7 @@ from gaze.pipes import Graph
 if __name__ == '__main__':
     x = NetworkSource()
     x = FaceRecognition()(x)
-    x = FileSink()(x)
+    x = AutoVideoSink()(x)
 
     graph = Graph(x)
     graph.run()
