8d5efc6dcf457d163d273274d6222c98711fe2a9
Jingyi Zhu <t-jinzhu@microsoft.com>
2018-11-12 17:59:50 +0800

    import source node

--- demo-old.py
+++ demo-old.py
@@ -1,6 +1,6 @@
 #from gaze.nodes.core import EdgeDetection, AutoVideoSink, FaceDetection
 from gaze.pipes import Graph
-from gaze.nodes import VideoTestSource, FileSource, DefaultDeviceSource, NetworkSource
+from gaze.nodes import VideoTestSource, FileSource, DefaultDeviceSource, NetworkSource, UdpSource
 from gaze.nodes import FileSink, UdpSink, AutoVideoSink
 
 x = UdpSource(port=5423)
